88c76b07e02695f867cdd75af6d5fce0028e5b1b
Wissam Jarjoui <bosswissam@gmail.com>
2012-01-30 08:28:08 -0500

    added ResultUser

---
+++ Thesis/main/utils.py
@@ -0,0 +1,19 @@
+'''
+Created on Jan 30, 2012
+
+@author:  Wissam Jarjoui (wjarjoui@mit.edu)
+'''
+from db import models
+from django.contrib.auth.models import User
+
+class ResultUser(object):
+    username = models.CharField()
+    first_name = models.CharField()
+    last_name = models.CharField()
+    location = models.CharField()
+    
+    def __init__(self, user_profile, user):
+        username = user.username
+        first_name = user.first_name
+        last_name = user.last_name
+        location = user_profile.location
