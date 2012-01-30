7d6289f5593a3906b4a4641412a8c8decf14084b
Sinchan Banerjee <banerjee.sinchan@gmail.com>
2012-01-29 23:29:52 -0500

    adding files

--- Thesis/views.py
+++ Thesis/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
-
+from django.shortcuts import render_to_response
 
 def start(request):
 	if request.user.is_authenticated():
 	    # TODO: show profile
-		return HttpResponse("TODO: show profile")
+		return render_to_response('pageTemplates/profile.html', {'pages': ['Play', 'Discover', 'Help a Puzzlaefer'], 'current_page': 'Play' })
 	else:
 	    return HttpResponseRedirect('/accounts/login/')
     
