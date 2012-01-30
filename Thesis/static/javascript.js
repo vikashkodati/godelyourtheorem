e4274bc9032bba15bd122d5d91df0a372cebe7db
Sinchan Banerjee <banerjee.sinchan@gmail.com>
2012-01-30 06:31:59 -0500

    placeholders added [html5]

--- Thesis/static/javascript.js
+++ Thesis/static/javascript.js
@@ -56,17 +56,32 @@ function change_page(event){
 }
 
 $(document).ready(function() {
-	$(".nav-page-element").click(change_page);
+	if($(".nav-page-element")){
+		$(".nav-page-element").click(change_page);
+	}
+	
+	if ($(".registrationForm")) {
+		var inputs = $(".form > li > input");
+		var labels = $(".form > li > label");
+		for (i=0; i< inputs.length; i++){
+			$(inputs[i]).attr("placeholder",$(labels[i]).html().replace(":", "..."));
+			$(labels[i]).remove();
+		}
+		$(labels[inputs.length]).remove();
+	}
+	
+	
+	if (document.getElementById('id_location')){
+		var defaultBounds = new google.maps.LatLngBounds(
+		  new google.maps.LatLng(-33.8902, 151.1759),
+		  new google.maps.LatLng(-33.8474, 151.2631));
 
-	var defaultBounds = new google.maps.LatLngBounds(
-	  new google.maps.LatLng(-33.8902, 151.1759),
-	  new google.maps.LatLng(-33.8474, 151.2631));
+		var input = document.getElementById('id_location');
+		var options = {
+		  bounds: defaultBounds,
+		  types: ['geocode']
+		};
 
-	var input = document.getElementById('id_location');
-	var options = {
-	  bounds: defaultBounds,
-	  types: ['geocode']
-	};
-
-	autocomplete = new google.maps.places.Autocomplete(input, options);
+		autocomplete = new google.maps.places.Autocomplete(input, options);
+	}
 });
