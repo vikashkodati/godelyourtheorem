c3f9a7810de6e5af4836159398a2d7278d268652
Sinchan Banerjee <banerjee.sinchan@gmail.com>
2012-01-30 06:03:03 -0500

    added Google Maps Location AutoComplete

--- Thesis/static/javascript.js
+++ Thesis/static/javascript.js
@@ -3,19 +3,6 @@ function test(){
 	Dajax.process();
 }
 
-function change_page(event){
-	var pageClickedTarget = $(event.target);
-	var pageClicked = pageClickedTarget.html();
-	var currentPage = $('.selected').html();
-	if (currentPage != pageClicked) {
-		$('.selected').removeClass('selected');
-		pageClickedTarget.addClass('selected');
-		Dajaxice.Thesis.main.changePage(function(data){
-			Dajax.process(data);
-		},{'newPage':pageClicked});
-	}
-}
-
 $.fn.serializeObject = function()
 {
     var o = {};
@@ -38,6 +25,48 @@ function send_form(){
 	Dajaxice.Thesis.main.send_form(test,{'form':data});
 }
 
+var initiate_play_search = function(){
+	var defaultBounds = new google.maps.LatLngBounds(
+	  new google.maps.LatLng(-33.8902, 151.1759),
+	  new google.maps.LatLng(-33.8474, 151.2631));
+
+	var input = document.getElementById('search-input');
+	var options = {
+	  bounds: defaultBounds,
+	  types: ['geocode']
+	};
+
+	autocomplete = new google.maps.places.Autocomplete(input, options);
+}
+
+function change_page(event){
+	var pageClickedTarget = $(event.target);
+	var pageClicked = pageClickedTarget.html();
+	var currentPage = $('.selected').html();
+	if (currentPage != pageClicked) {
+		$('.selected').removeClass('selected');
+		pageClickedTarget.addClass('selected');
+		Dajaxice.Thesis.main.changePage(function(data){
+			Dajax.process(data);
+			if (pageClicked == "Play"){
+				initiate_play_search();
+			}
+		},{'newPage':pageClicked});
+	}
+}
+
 $(document).ready(function() {
-  $(".nav-page-element").click(change_page);
+	$(".nav-page-element").click(change_page);
+
+	var defaultBounds = new google.maps.LatLngBounds(
+	  new google.maps.LatLng(-33.8902, 151.1759),
+	  new google.maps.LatLng(-33.8474, 151.2631));
+
+	var input = document.getElementById('id_location');
+	var options = {
+	  bounds: defaultBounds,
+	  types: ['geocode']
+	};
+
+	autocomplete = new google.maps.places.Autocomplete(input, options);
 });
